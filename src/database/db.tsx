9bd5b6dad73b042f10346578b6de69f4d9ef25aa
daveB4r <davidpinedaaaa@gmail.com>
2023-12-23 19:33:31 -0500

    fixing database

--- src/database/db.tsx
+++ src/database/db.tsx
@@ -1,15 +1,7 @@
 import mysql from 'mysql2/promise';
 
-let connection = {
-  host: 'btzph0wi43nanhkkc11s-mysql.services.clever-cloud.comv',
-  user: 'u3qtqosmeyje9hjb',
-  password: 'kJ54c9vx3HCjPKVaNFXl',
-  database: 'btzph0wi43nanhkkc11s',
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0,
-};
-/*if(process.env.NODE_ENV === 'development'){
+let connection;
+if(process.env.NODE_ENV === 'development'){
   connection = {
     host: process.env.DB_DEV_HOST,
     user: process.env.DB_DEV_USER,
@@ -29,7 +21,7 @@ let connection = {
     connectionLimit: 10,
     queueLimit: 0,
   }
-}*/
+}
 const pool = mysql.createPool(connection);
 
 export default pool;
