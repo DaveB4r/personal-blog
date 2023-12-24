fa691be401d4cfe79dd2033ff839ea48c44876f1
daveB4r <davidpinedaaaa@gmail.com>
2023-12-23 19:20:07 -0500

    fixing database

--- src/database/db.tsx
+++ src/database/db.tsx
@@ -1,7 +1,15 @@
 import mysql from 'mysql2/promise';
 
-let connection: any;
-if(process.env.NODE_ENV === 'development'){
+let connection = {
+  host: process.env.DB_PROD_HOST,
+  user: process.env.DB_PROD_USER,
+  password: process.env.DB_PROD_PASSWORD,
+  database: process.env.DB_PROD,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+};
+/*if(process.env.NODE_ENV === 'development'){
   connection = {
     host: process.env.DB_DEV_HOST,
     user: process.env.DB_DEV_USER,
@@ -21,7 +29,7 @@ if(process.env.NODE_ENV === 'development'){
     connectionLimit: 10,
     queueLimit: 0,
   }
-}
+}*/
 const pool = mysql.createPool(connection);
 
 export default pool;
