1c7e8f8e1866978886884eac2a7d5b61132f6daf
daveB4r <davidpinedaaaa@gmail.com>
2023-12-23 20:29:02 -0500

    setting the domain

--- src/app/actions/FetchPosts.tsx
+++ src/app/actions/FetchPosts.tsx
@@ -1,6 +1,6 @@
 import { Posts } from "@/interfaces/PostsInterface";
 export async function FetchPosts(limit: string, category?: string, search?: string) {
-  let apiUrl = `${process.env.NODE_ENV === 'development' ? "http://localhost:3000":"https://personal-blog-ddn63y4ps-juan-david-pinedas-projects.vercel.app"}/api/posts?limit=${limit}`;
+  let apiUrl = `${process.env.NODE_ENV === 'development' ? "http://localhost:3000":"https://personal-blog-five-inky.vercel.app"}/api/posts?limit=${limit}`;
   if(category) apiUrl += `&category=${category}`;
   if(search) apiUrl += `&search=${search}`;
   try {
