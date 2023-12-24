bed86f13263e856de545c6b7674c7ca6c23191f0
daveB4r <davidpinedaaaa@gmail.com>
2023-12-23 19:06:45 -0500

    fixing favorites

--- src/app/api/posts/favorites/get/route.ts
+++ src/app/api/posts/favorites/get/route.ts
@@ -1,14 +1,19 @@
 import pool from "@/database/db";
 import { NextResponse } from "next/server";
 
-export async function GET(req: Request): Promise<NextResponse>{
+export async function GET(req: Request): Promise<Response> {
   try {
     const connection = await pool.getConnection();
-    const {searchParams} = new URL (req.url);
+    const { searchParams } = new URL(req.url);
     const ids = searchParams.get("ids");
-    const [rows] = await connection.query(`SELECT * FROM posts WHERE id IN (${ids})`);
+    const [rows] = await connection.query(
+      `SELECT * FROM posts WHERE id IN (${ids})`
+    );
     connection.release();
-    return NextResponse.json({posts: rows});
+    return new Response(JSON.stringify({ posts: rows }), {
+      status: 200,
+      headers: { "Content-Type": "application/json" }
+    });
   } catch (e) {
     console.error(e);
   }
